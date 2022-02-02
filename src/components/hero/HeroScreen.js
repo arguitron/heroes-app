5b94e98d7c0a29a9d739034e577ad894190f942f
Abel Guitron <guitronaca@gmail.com>
2022-02-01 22:49:09 -0600

    :zap: Adding a use memo for the getHero methods.
    also some animations.

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../selectors/getHeroById";
 import { Navigate } from "react-router-dom";
@@ -6,7 +6,7 @@ import { Navigate } from "react-router-dom";
 export const HeroScreen = () => {
   const { heroId } = useParams();
   const navigate = useNavigate();
-  const hero = getHeroById(heroId);
+  const hero = useMemo(() => getHeroById(heroId), [heroId]);
   if (!hero) {
     return <Navigate to="/" />;
   }
@@ -18,35 +18,33 @@ export const HeroScreen = () => {
   const { id, superhero, publisher, alter_ego, first_appearance, characters } =
     hero;
   return (
-    <>
-      <div className="row mt-5">
-        <div className="col-4">
-          <img
-            src={`/assets/${id}.jpg`}
-            alt={superhero}
-            className="img-thumbnail"
-          />
-        </div>
-        <div className="col-8">
-          <h3>{superhero}</h3>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <b>Alter ego: </b> {alter_ego}
-            </li>
-            <li className="list-group-item">
-              <b>Publisher: </b> {publisher}
-            </li>
-            <li className="list-group-item">
-              <b>First Appearance: </b> {first_appearance}
-            </li>
-          </ul>
-          <h5 className="mt-3">Characters</h5>
-          <p>{characters}</p>
-          <button onClick={handleReturn} className="btn btn-outline-info">
-            Regresar
-          </button>
-        </div>
+    <div className="row mt-5 animate__animated animate__bounceInLeft">
+      <div className="col-4 animate__animated animate__bounceInLeft">
+        <img
+          src={`/assets/${id}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
       </div>
-    </>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance: </b> {first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+        <button onClick={handleReturn} className="btn btn-outline-info">
+          Regresar
+        </button>
+      </div>
+    </div>
   );
 };
