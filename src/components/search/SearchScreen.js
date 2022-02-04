1acb4d8d895220aee648f525bf62fecacf3bd61a
Abel Guitron <guitronaca@gmail.com>
2022-02-03 23:52:55 -0600

    :sparkles: Search Hero by name.

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { heroes } from "../../data/heroes";
+import { HeroCard } from "../hero/HeroCard";
+import { useForm } from "../hooks/useform";
+import { getHeroesByName } from "../selectors/getHeroesByName";
+import queryString from "query-string";
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+  const [values, handleInputChange] = useForm({
+    searchText: q,
+  });
+  const heroesFileted = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`?q=${searchText}`);
+  };
+
+  const { searchText } = values;
   return (
     <>
-      <h1>SearchScreen</h1>
+      <h1>Búsquedas</h1>
       <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Buscar un héroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button
+              className="btn btn-outline-primary col-12 mt-2"
+              type="submit"
+            >
+              Buscar...
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+          {q === "" ? (
+            <div className="alert alert-info">Búscar un héroe</div>
+          ) : (
+            heroesFileted.length === 0 && (
+              <div className="alert alert-danger">
+                No hay resultados de: {q}
+              </div>
+            )
+          )}
+          {heroesFileted.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
